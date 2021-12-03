56d4dd76ba736fb7af2a6fe72da0f50256c53e03
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-02 23:42:38 -0800

    add request param

--- src/main/java/com/vcabading/hellohuman/HomeController.java
+++ src/main/java/com/vcabading/hellohuman/HomeController.java
@@ -1,13 +1,18 @@
 package com.vcabading.hellohuman;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "Hello";
+	public String index(@RequestParam(value="name", required=false) String searchName) {
+		if (searchName == null) {
+			return "Hello Human";
+		} else {
+			return "Hello " + searchName;
+		}
 	}
 }
